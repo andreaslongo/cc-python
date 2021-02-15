8f844d839400531e06b592e59389108ebdb8f8e0
Andreas Longo <git@andreaslongo.com>
2021-02-15 18:35:21 +0100

    Change default Python package name to match the project repository

--- {{cookiecutter.project_repository}}/setup.py
+++ {{cookiecutter.project_repository}}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="{{ cookiecutter.project_slug }}",
+    name="{{ cookiecutter.project_repository }}",
     package_dir={"": "src"},
     packages=find_packages(where="src")
 )
