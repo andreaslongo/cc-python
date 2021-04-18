94dda48252acd4c664ad9f6ce0144ed7cfd8f35e
Andreas Longo <git@andreaslongo.com>
2021-04-18 16:21:32 +0200

    Add basic python version restriction

--- {{cookiecutter.project_repository}}/setup.py
+++ {{cookiecutter.project_repository}}/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="{{ cookiecutter.project_repository }}",
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
+    python_requires=">=3'
 )
